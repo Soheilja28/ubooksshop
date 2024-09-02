7738bb5de76696144b575883c122b8365a31f660
Soheilja28 <$sohrabjalalitr28@gmail.com>
2024-09-02 14:54:04 +0330

    Update

--- components/Pages/Block.tsx
+++ components/Pages/Block.tsx
@@ -2,79 +2,78 @@ export const Block = props => {
     if (!props.state.fav) {
         props.state.fav = []
     }
-  
+
     if (!props.state.cart) {
         props.state.cart = []
     }
-    
-    return <f-x style={{
-        width: 150,
-        height: 290,
-        flex: 1,
-        objectFit: "fill",
-        position: "relative",
-    }}>
 
+    return <c-c class={global.styles.hoverzoom_nofade}
+        onClick={() => {
+            props.state.form = "bookspecs"
+            props.state.book = props.book
+            props.refresh()
 
-        <c-x class={global.styles.hoverzoom_nofade} style={{
-            width: 150,
-            height: 260,
+        }}
+        style={{
+            width: 160,
+            height: 270,
+            minWidth: 160,
+            borderBottomLeftRadius: 15,
+            borderBottomRightRadius: 15,
+            position: "relative",
             backgroundColor: "white",
             boxShadow: "0 0 9px black",
-            borderBottomLeftRadius: 10,
-            borderBottomRightRadius: 10,
-            marginTop:5
+            marginTop: 20
+
         }}>
 
-            <img
-                src={props.book.imageLink} style={{
-                    width: 150,
-                    height: 200,
-                    flex: 1,
-                    objectFit: "fill",
-                    minWidth: 150,
-                    gap: 10,
-
-                }}
-                onClick={() => {
-                    props.state.form = "bookspecs"
-                    props.state.book = props.book
-                    props.refresh()
-
-                }} />
-
-            <br-xx />
-            <br-xxx />
-            <br-xxx />
-            <br-xxx />
-
-            <w-cc style={{ fontSize: 11, color: "darkblue", direction: "ltr" }}>{props.book.title}</w-cc>
-
-            <br-xx />
-            <br-xx />
-            <br-xx />
-
-            <f-cc><hr style={{ width: 120 }} /></f-cc>
-
-            <br-xx />
-
-            <w-cc><del>{(props.book.price * 1.2 as number).toLocaleString("fa-IR")}</del></w-cc>
-            <w-cc>{(props.book.price as number).toLocaleString("fa-IR")} تومان</w-cc>
-
-            <br-x />
-            <br-x />
-            <br-x />
-
-            {props.state.fav.includes(props.book.title) ? <img src="https://irmapserver.ir/research/12/heart.png"
-                style={{ width: 30, height: 30, position: "absolute", marginTop: 225, marginRight: 8 }} /> : null}
-
-            {props.state.cart.includes(props.book.title) ? <img src="https://irmapserver.ir/research/12/check%20%282%29.png"
-                style={{ height: 30, width: 30, position:"absolute", marginRight:110, marginTop:225 }} /> : null}
-
-        </c-x>
+        <img
+            src={props.book.imageLink} style={{
+                width: 150,
+                height: 190,
+                minWidth: 150,
+                marginTop: 5,
+                objectFit: "fill"
+            }}
+        />
 
 
 
-    </f-x>
+        <br-xx />
+        <br-xx />
+        <br-xxx />
+        <br-xxx />
+
+        <w-cc style={{ fontSize: 11, color: "darkblue", direction: "ltr" }}>{props.book.title}</w-cc>
+
+        <br-xx />
+        <br-xx />
+        <br-xx />
+
+        <f-cc><hr style={{ width: 120, opacity: 0.7 }} /></f-cc>
+
+        <br-xx />
+
+        <w-cc style={{color:"red"}}><del>{(props.book.price * 1.2 as number).toLocaleString("fa-IR")}</del></w-cc>
+        <w-cc>{(props.book.price as number).toLocaleString("fa-IR")} تومان</w-cc>
+
+        <br-x />
+        <br-x />
+        <br-x />
+
+        {props.state.fav.includes(props.book.title) ? <img src="https://cdn.turing.team/research/12/heart%20main.png"
+            style={{ width: 28, height: 28, position: "absolute", top: 230, right: 8 }} /> : null}
+
+        {props.state.cart.includes(props.book.title) ? <img src="https://cdn.turing.team/research/12/check%20%282%29.png"
+            style={{ height: 30, width: 30, position: "absolute", top: 230, right: 120 }} /> :
+            <img src='https://cdn.turing.team/research/12/add-to-cart.png' style={{ height: 30, width: 30, position: "absolute", top: 230, right: 120 }} />
+
+        }
+
+    </c-c>
+
+
+
+
 
 }
--- components/Pages/index.tsx
+++ components/Pages/index.tsx
@@ -9,21 +9,31 @@ import css from './css.module.css'
 import './css.module.css'
 import WindowFloat from '../Libs/WindowFloat';
 import { Block } from './Block';
+import { stat } from 'node:fs';
 
 
 
 export default p => Component(p, Page);
 const Page: PageEl = (props, state, refresh, getProps) => {
+  getProps(async () => {
+    let cart = localStorage.getItem("cart")
+    if (cart) {
+      state.cart = JSON.parse(cart)
+    }
+
+  })
 
   let styles = global.styles
   // let name = "خوش آمدید"
   // ------------------------------------------------------------------------------
 
   state.cart = Array.from(new Set(state.cart))
+  //state.fav = Array.from(new Set(state.fav))
 
 
   let totalprice = 0;
   let count = 0;
+  //let favarite = 0
 
   for (let cart of state.cart) {
 
@@ -34,6 +44,10 @@ const Page: PageEl = (props, state, refresh, getProps) => {
     count += 1;
 
   }
+  // ------------------------------------
+  // for (let fav of state.fav) {
+  //   favarite += 1;
+  // }
 
   return (
     <div style={{ direction: "rtl", minHeight: "11vh", }}>
@@ -42,9 +56,19 @@ const Page: PageEl = (props, state, refresh, getProps) => {
       <Window title='سبد خرید' style={{ height: 120, margin: 10, width: "calc(100% - 20px)", backgroundColor: "gainsboro" }}>
 
         <c-x style={{ position: "relative" }}>
+
+          {/* <f-x>
+            <f-cc style={{ margin: 10, paddingRight: 10 }}>
+              <f-12 style={{ fontSize: 20 }}>تعداد علاقه مندی : </f-12>
+              <f-12 style={{ fontSize: 20 }}>{(favarite as number).toLocaleString("fa-IR")}</f-12>
+            </f-cc>
+
+          </f-x> */}
+
+
           <f-x>
             <f-cc style={{ margin: 10, paddingRight: 10 }}>
-              <f-12><img src='https://irmapserver.ir/research/12/shopping-cart%20%28m%29.png' style={{ width: 30, height: 30 }} /></f-12>
+              <f-12><img src='https://cdn.turing.team/research/12/shopping-cart%20%28m%29.png' style={{ width: 30, height: 30 }} /></f-12>
               <sp-2 />
               <f-12 style={{ fontSize: 20 }}>تعداد کتاب : </f-12>
               <f-12 style={{ fontSize: 20 }}>{(count as number).toLocaleString("fa-IR")}</f-12>
@@ -54,7 +78,7 @@ const Page: PageEl = (props, state, refresh, getProps) => {
 
           <f-x>
             <f-cc style={{ fontSize: 20, paddingRight: 10 }}>
-              <f-12><img src='https://irmapserver.ir/research/12/dollars.png' style={{ width: 30, height: 30 }} /></f-12>
+              <f-12><img src='https://cdn.turing.team/research/12/dollars.png' style={{ width: 30, height: 30 }} /></f-12>
               <sp-2 />
               <f-12 style={{ fontSize: 20 }}>مجموع قابل پرداخت:<b></b>  </f-12>
               <sp-2 />
@@ -65,7 +89,7 @@ const Page: PageEl = (props, state, refresh, getProps) => {
             </f-cc>
 
           </f-x>
-          <img src='https://irmapserver.ir/research/12/stack-of-books.png' style={{ width: 80, height: 80, position: "absolute", right: 800, top: 7 }} />
+          <img src='https://cdn.turing.team/research/12/stack-of-books.png' style={{ width: 80, height: 80, position: "absolute", right: 800, top: 7 }} />
 
         </c-x>
 
@@ -79,22 +103,22 @@ const Page: PageEl = (props, state, refresh, getProps) => {
         delete state.form
         refresh()
       }}>
-        <c-x style={{position:"relative"}}>
+        <c-x style={{ position: "relative" }}>
           <f-c>
             <f-15>نام کتاب:</f-15>
             <sp-2 />
             <f-15>{state.book.title}</f-15>
           </f-c>
 
-          <br-xx/>
+          <br-xx />
 
           <f-c>
             <f-15>کشور:</f-15>
             <sp-2 />
             <f-15>{state.book.country}</f-15>
           </f-c>
-          
-          <br-xx/>
+
+          <br-xx />
 
           <f-c>
             <f-15>زبان:</f-15>
@@ -102,7 +126,7 @@ const Page: PageEl = (props, state, refresh, getProps) => {
             <f-15>{state.book.language}</f-15>
           </f-c>
 
-          <br-xx/>
+          <br-xx />
 
           <f-c>
             <f-15>صفحه:</f-15>
@@ -110,7 +134,7 @@ const Page: PageEl = (props, state, refresh, getProps) => {
             <f-15>{(state.book.pages as number).toLocaleString("fa-IR")}</f-15>
           </f-c>
 
-          <br-xx/>
+          <br-xx />
 
           <f-c>
             <f-15>قیمت:</f-15>
@@ -123,48 +147,77 @@ const Page: PageEl = (props, state, refresh, getProps) => {
 
 
 
-        <img src='https://irmapserver.ir/research/12/book.png' style={{ width: 80, height: 80, position:"absolute", right:325 , bottom:70}} />
+        <img src='https://cdn.turing.team/research/12/book.png' style={{ width: 80, height: 80, position: "absolute", right: 325, bottom: 70 }} />
 
         <br-x />
         <br-x />
 
         <f-cse style={{ gap: 10 }}>
-          <g-b style={{ height: 40, backgroundColor: "darkblue", color: "white" }} onClick={() => {
-            if (!state.fav) {
-              state.fav = []
-              state.form = null
-            }
+          <g-b style={{
+            height: 40,
+            backgroundColor: state.fav.includes(state.book.title) ?
+              "indianred" : "darkblue",
+            color: "white"
+          }}
+            onClick={() => {
+              if (!state.fav) {
+                state.fav = []
+                state.form = null
+              }
+              if (state.fav.includes(state.book.title)) {
+                state.fav = state.fav.filter(item => item !== state.book.title)
+                state.form = null
+              }
+              else {
+                state.fav.push(state.book.title)
+                state.form = null
+              }
 
-            state.fav.push(state.book.title)
-            state.form = null
-            refresh()
-          }}>
 
-            <f-12>افزودن به علاقه مندی ها</f-12>
+              refresh()
+            }}>
+
+            {state.fav.includes(state.book.title) ?
+              <f-cc><img src='https://cdn.turing.team/research/12/delete-heart.png' style={{ width: 30, height: 30 }} /></f-cc> : <f-cc><img src='https://cdn.turing.team/research/12/heart.png' style={{ width: 30, height: 30 }} /></f-cc>}
             <sp-3 />
-            <f-cc><img src="https://irmapserver.ir/research/12/heart.png" style={{ width: 30, height: 30 }} /></f-cc>
+            {state.fav.includes(state.book.title) ?
+              <f-12>حذف از علاقه مندی ها</f-12> : <f-12>افزودن به علاقه مندی ها</f-12>}
 
           </g-b>
 
 
-          <g-b style={{ height: 40, backgroundColor: "darkblue", color: "white" }} onClick={() => {
-            if (!state.cart) {
-              state.cart = []
-            }
-            if (state.cart.includes(state.book.title)) {
-              state.cart = state.cart.filter(item => item !== state.book.title)
-              state.form = null
-            }
-            else {
-              state.cart.push(state.book.title)
-              state.form = null
+          <g-b style={{
+            height: 40,
+            backgroundColor: state.cart.includes(state.book.title) ?
+              "indianred" : "darkblue",
+            color: "white"
+          }}
+            onClick={async () => {
+              if (!state.cart) {
+                state.cart = []
+              }
+              if (state.cart.includes(state.book.title)) {
+                state.cart = state.cart.filter(item => item !== state.book.title)
+                localStorage.setItem("cart", JSON.stringify(state.cart))
+                state.form = null
+                refresh()
+                // alert(" کتاب" + " " + state.book.title + " " + " از سبد خرید شما حذف گردید")
+                await api("/api/test", state.cart)
+              }
+              else {
+                state.cart.push(state.book.title)
+                localStorage.setItem("cart", JSON.stringify(state.cart))
+                state.form = null
+                refresh()
+              }
 
-            }
-            refresh()
-          }}>
-            <f-12>افزودن به سبد خرید</f-12>
-            <sp-3 />
-            <f-cc><img src='https://irmapserver.ir/research/12/cart.png' style={{ width: 30, height: 30 }} /></f-cc>
+            }}>
+
+            {state.cart.includes(state.book.title) ?
+              <f-cc><img src='https://cdn.turing.team/research/12/delete-product.png' style={{ width: 30, height: 30 }} /></f-cc> : <f-cc><img src='https://cdn.turing.team/research/12/cart.png' style={{ width: 30, height: 30 }} /></f-cc>}
+            <sp-2 />
+            {state.cart.includes(state.book.title) ?
+              <f-13>حذف از سبد خرید</f-13> : <f-13>افزودن به سبد خرید</f-13>}
           </g-b>
         </f-cse>
 
@@ -201,7 +254,7 @@ export async function getServerSideProps(context) {
   let books = await global.db.collection("Books").find({}).toArray()
 
   for (let book of books) {
-    book.imageLink = "https://irmapserver.ir/research/ex/books/" + book.imageLink
+    book.imageLink = "https://cdn.turing.team/research/ex/books/" + book.imageLink
   }
   console.log(books)
 
